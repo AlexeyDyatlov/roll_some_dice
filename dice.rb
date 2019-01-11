2e2b23971d9fd4fd6df077d7837686fcfc1ff56d
spec2pc <C:\Users\spec2pc.PULS\AppData\Roaming\The Bat!>
2019-01-11 11:08:53 +0300

    Ver. 2

--- dice.rb
+++ dice.rb
@@ -2,6 +2,21 @@ puts "how many dice?"
 
 num = gets.to_i
 
-num.times do
-  puts rand(6) + 1
+def show_rolling_die
+  100.times do
+    # Вместо puts используем print, т.к. он не добавляет перенос строки в конце
+    print "#{rand(6) + 1}\r"
+    sleep 0.01
+  end
 end
+
+
+sum = 0
+num.times do |i|
+  show_rolling_die
+  die = rand(6) + 1
+  puts die
+  sum += die
+end
+
+puts "Sum of dice: #{sum}"
